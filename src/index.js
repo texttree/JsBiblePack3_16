6d31edc7aad078f3b50ab5aac98a851f0887f44b
DenisArger <den.arger@gmail.com>
2023-05-22 14:26:37 +0300

    run test project webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const i = 1;
+const j = 1;
+console.log(i + j);
+
+import "./styles/styles.css";
+import json from "./assets/json.json";
+import WebPackLogo from "./assets/webpack-logo.png";
+
+console.log("JSON", json);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HTMLWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
