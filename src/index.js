549736c794e0e3932b484601da87b979efaf5c42
DenisArger <den.arger@gmail.com>
2023-05-26 15:23:06 +0300

    Тестовая разработка

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
   download.addEventListener("click", () => {
     const link = document.createElement("a");
     const zipLink =
-      "https://app.thedigitalbiblelibrary.org/entry/download_archive?id=f6c04124034a114e&license=30235&revision=1&type=release";
+      "https://app.thedigitalbiblelibrary.org/entry/download_archive?id=d8bb452293f21682&license=30235&revision=1&type=release";
     const zipName = "test.zip";
 
     link.setAttribute("href", zipLink);
