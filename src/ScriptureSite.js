a42eba1e0d4b3cb423038785d98bbc36ec47ce40
DenisArger <den.arger@gmail.com>
2023-06-26 16:26:09 +0300

    url забираем из localStorage

--- src/ScriptureSite.js
+++ src/ScriptureSite.js
@@ -5,6 +5,7 @@ class ScriptureSite {
   constructor(statusSrc) {
     this.apiInterface = new ApiInterface();
     this.progressStatus = new ProgressStatus(statusSrc);
+    this.urlSite = `https://${localStorage.urlSite}`;
   }
 
   async getBasic(url) {
@@ -16,28 +17,28 @@ class ScriptureSite {
     }
   }
   async getAllBooks(bibleVersion) {
-    const URL = `https://api.scripture.api.bible/v1/bibles/${bibleVersion}/books?include-chapters=true&include-chapters-and-sections=false`;
+    const URL = `${this.urlSite}/bibles/${bibleVersion}/books?include-chapters=true&include-chapters-and-sections=false`;
 
     const data = await this.getBasic(URL);
     return data.data;
   }
 
   async getAllChapters(bibleVersion, book) {
-    const URL = `https://api.scripture.api.bible/v1/bibles/${bibleVersion}/books/${book}/chapters`;
+    const URL = `${this.urlSite}/bibles/${bibleVersion}/books/${book}/chapters`;
 
     const data = await this.getBasic(URL);
     return data.data;
   }
 
   async getDataVerse(bibleVersion, range, typeContent) {
-    const URL = `https://api.scripture.api.bible/v1/bibles/${bibleVersion}/passages/${range}?content-type=${typeContent}&include-notes=false&include-titles=true&include-chapter-numbers=true&include-verse-numbers=true&include-verse-spans=false&use-org-id=false`;
+    const URL = `${this.urlSite}/bibles/${bibleVersion}/passages/${range}?content-type=${typeContent}&include-notes=false&include-titles=true&include-chapter-numbers=true&include-verse-numbers=true&include-verse-spans=false&use-org-id=false`;
 
     const data = await this.getBasic(URL);
     return data.data;
   }
 
   async getAllVerses(bibleVersion, chapterId) {
-    const URL = `https://api.scripture.api.bible/v1/bibles/${bibleVersion}/chapters/${chapterId}?content-type=html&include-notes=false&include-titles=true&include-chapter-numbers=true&include-verse-numbers=true&include-verse-spans=false`;
+    const URL = `${this.urlSite}/bibles/${bibleVersion}/chapters/${chapterId}?content-type=html&include-notes=false&include-titles=true&include-chapter-numbers=true&include-verse-numbers=true&include-verse-spans=false`;
 
     const data = await this.getBasic(URL);
     return data.data;
