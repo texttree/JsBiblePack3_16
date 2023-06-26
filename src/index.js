418dafc631d3fd3dfa90d6c686a1df65acadedbf
DenisArger <den.arger@gmail.com>
2023-06-26 16:24:32 +0300

    Добавлена инициализация urlSite в localStorage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import "./styles/styles.css";
 import { NeedBible } from "./NeedBible";
 
 window.addEventListener("DOMContentLoaded", () => {
+  const urlSrc = document.querySelector("#url-src");
   const keySrc = document.querySelector("#key-src");
   const paiSrc = document.querySelector("#paid-src");
   const revSrc = document.querySelector("#rev-src");
   const contentSrc = document.querySelector("#content-src");
   const statusSrc = document.querySelector("#status-src");
+
+  if (urlSrc.value === "" && localStorage.urlSite !== undefined) {
+    urlSrc.value = localStorage.urlSite;
+  }
   if (keySrc.value === "" && localStorage.apiKey !== undefined) {
     keySrc.value = localStorage.apiKey;
   }
@@ -17,6 +22,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const revision = revSrc.value;
     const typeContent = contentSrc.value;
     const bibleVersion = `${paid}-${revision}`;
+    localStorage.urlSite = urlSrc.value;
     localStorage.apiKey = keySrc.value;
 
     const needBible = new NeedBible(bibleVersion, typeContent, statusSrc);
